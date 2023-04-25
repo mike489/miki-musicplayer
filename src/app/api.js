d5b08eb7909e2f9667b55b0cc6d195093711b7fd
mike489 <michaeltesfaye84@gmail.com>
2023-04-25 13:37:12 +0300

    same simple edit on api.js

--- src/app/api.js
+++ src/app/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = "http://localhost:3001";
+const apiUrl = "https://localhost:3001";
 
 export const fetchSongsApi = async () => {
   const response = await axios.get(`${apiUrl}/songs/`);
