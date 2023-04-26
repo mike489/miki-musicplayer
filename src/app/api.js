e1df4a832eeb8bd924c25cf9af0dab20a7a2e0c8
mike489 <michaeltesfaye84@gmail.com>
2023-04-26 11:26:07 +0300

    delete

--- src/app/api.js
+++ src/app/api.js
@@ -21,11 +21,3 @@ export const deleteSongApi = async (songId) => {
   await axios.delete(`${apiUrl}/songs/${songId}/`);
 };
 
-server.listen(3001, "localhost"); // or server.listen(3001, '0.0.0.0'); for all interfaces
-server.on("listening", function () {
-  console.log(
-    "Express server started on port %s at %s",
-    server.address().port,
-    server.address().address
-  );
-});
